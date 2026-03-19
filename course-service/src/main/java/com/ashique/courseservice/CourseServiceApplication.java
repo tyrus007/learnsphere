1b98d359470554a8307ce85681fc1130e8edf8a8
tyrus007 <ashiqueulmustafa94@gmail.com.com>
2026-03-20 00:14:33 +0600

    fix: there was a misspelling in component-scan package that caused unauthorized error.

--- course-service/src/main/java/com/ashique/courseservice/CourseServiceApplication.java
+++ course-service/src/main/java/com/ashique/courseservice/CourseServiceApplication.java
@@ -2,11 +2,8 @@ package com.ashique.courseservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
-
-@ComponentScan(basePackages = {"com.ashique.courserservice", "com.ashique.learnsphere.jwt"})
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.ashique.courseservice", "com.ashique.learnsphere.jwt"})
 public class CourseServiceApplication {
 
 	public static void main(String[] args) {
