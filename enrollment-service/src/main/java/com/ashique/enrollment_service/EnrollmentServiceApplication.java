828a07118fac7dacbb19a0d5488188dc5e341b8a
tyrus007 <ashiqueulmustafa94@gmail.com.com>
2026-03-27 18:05:41 +0600

    Fix: EnrollmentServiceApplication could not find the jwt-common to create bean for jwt class
    - added explicit path in the application class

--- enrollment-service/src/main/java/com/ashique/enrollment_service/EnrollmentServiceApplication.java
+++ enrollment-service/src/main/java/com/ashique/enrollment_service/EnrollmentServiceApplication.java
@@ -3,7 +3,10 @@ package com.ashique.enrollment_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {
+		"com.ashique.enrollment_service",
+		"com.ashique.learnsphere.jwt"
+})
 public class EnrollmentServiceApplication {
 
 	public static void main(String[] args) {
